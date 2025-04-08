63d2c2320ff4b631a8593bff873be5ac9650dd0d
hungcoder03 <hungcoder03@gmail.com>
2025-04-08 17:03:53 +0700

    Thiếu quy trình ban hang

--- src/main/java/com/adc/project_a/controller/CartController.java
+++ src/main/java/com/adc/project_a/controller/CartController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/cart")
+public class CartController {
 
     @GetMapping("")
-    public String home() {
-        return "index";
+    public String cart() {
+        return "cart";
     }
 
 }
